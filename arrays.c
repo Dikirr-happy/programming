13ccd72c2bd601a542f25979e4e315e5c2e39db2
Dikirr-happy <92512424+Dikirr-happy@users.noreply.github.com>
2021-12-10 09:45:41 +0300

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    int i;
+    for (i=0; i<5; i++)
+    {
+        printf("\n Enter a[%d]", i);
+        scanf("\n %d", &a[i]);
+    }
+    for (i=0; i<5; i++)
+    {
+        printf("\n a[%d]=%d", i, a[i]);
+    }
+    return 0;
+}
